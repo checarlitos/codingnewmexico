7fa143b150af64b1217e20a617525d957997fc0a
Carlos Beraun <cberaun2@cnm.edu>
2015-10-26 23:20:53 -0600

    Added index page for PWP codingnewmexico

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body>
 	<p>Milestone 1.</p>
-	<p>This site will serve two purposes. The site will bring to life my abilities and skills i have learned by displaying them in a professional platform as a business.</p>
+	This site will serve two purposes. The site will bring to life my abilities and skills i have learned by displaying them in a professional platform as a business.
 		<br/>The site will also isync be a live, current and functional portfolio/resume to help me earn a position in the right company.
 
 		My site will be a business(i don’t know what yet) that will attracted potential customers and clients to obtain my services. My Site will also be developed to showcase my coding for people who might be looking to hire me and would use my site as a reference point for what i may be capable of coding.
