a41d9c9c27d3208b8aeeaaff184ab9584a61e1cc
Carlos Beraun <cberaun2@cnm.edu>
2015-10-26 23:26:45 -0600

    Added index page for PWP codingnewmexico

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 		<br/>The site will also isync be a live, current and functional portfolio/resume to help me earn a position in the right company.
 		<br/><p>My site will be a business(i don’t know what yet) that will attracted potential customers and clients to obtain my services.
 		<br/>My Site will also be developed to showcase my coding for people who might be looking to hire me and would use my site as a
-		<br/>reference point for what i may be capable of coding.
+		<br/>reference point for what i may be capable of coding.</p>
 
 		I would like to be able to successfully produce a functional and interactive site that will be a useful and helpful tool for helping me pave a career path.
 	</p>
