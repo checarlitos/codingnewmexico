2d42b4b9f65f29de4dcf100665ecebf135167e15
Carlos Beraun <cberaun2@cnm.edu>
2015-11-13 00:30:02 -0700

    Added container size to page ---> index page to PWP

--- index.php
+++ index.php
@@ -51,10 +51,12 @@
     </nav>
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="col_lg_6"
     <div class="jumbotron">
       <h1>Jumbotron heading</h1>
       <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
       <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+    </div>
     </div>
 
     <div class="container">
