d319c99d054ccd66ca19399f081086d6127226ab
Carlos Beraun <cberaun2@cnm.edu>
2015-11-17 11:55:23 -0700

    Added bootstrap and started to set up header --->(milestone-2.php)

--- milestone-2.php
+++ milestone-2.php
@@ -3,10 +3,40 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta http-equiv="x-ua-compatible" content="IE=edge">
-		<meta name="viewport" vontent"width=device-width, >
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description">
+		<meta name="author">
+
+				<link rel="icon" href="../../favicon.ico">
+
+		<title> Coding New Mexico</title>
+
+				<!-- Bootstrap core CSS -->
+				<!-- Latest compiled and minified CSS -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
+				<!-- Optional theme -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+
+				<!-- Font Awsome----->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+				<!-- Latest compiled and minified JavaScript -->
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
 	</head>
 
 	<body>
-		<h2>Milestone 2.</h2>
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only"> </span>
+						</button>
+							<a class="navbar=brand" href="milestone-2.php">MILEstone-2</a>
+
+					</div>
+				</div>
+			</nav>
 	</body>
 </html>
