c245e388494f7446469ae27477869b2ad294b9fb
Carlos Beraun <cberaun2@cnm.edu>
2015-11-16 23:47:51 -0700

    Added container size to page ---> index page to PWP

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
-      <div class="container" class="align-center">
+      <div class="container,align-center" >
         <h1>Hello, world!</h1>
         <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
       </div>
