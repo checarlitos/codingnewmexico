31075b0ee31d954646c30b6cd0c9ed684a94da3b
Carlos Beraun <cberaun2@cnm.edu>
2015-11-19 19:48:25 -0700

    Added custom css link --->(milestone-2.php)

--- milestone-2.php
+++ milestone-2.php
@@ -22,7 +22,7 @@
 				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 				<!--custom css------>
-				<link rel="stylesheet" href="//css/miletwo.css
+				<link rel="stylesheet" href="//css/miletwo.css">
 
 				<!-- Latest compiled and minified JavaScript -->
 				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
