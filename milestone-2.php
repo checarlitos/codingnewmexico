bbc001b94a587c4b898fcdbc162f67fb2b826b6d
Carlos Beraun <cberaun2@cnm.edu>
2015-11-19 18:30:41 -0700

    deleted and added new block of code for person. --->(milestone-2.php)

--- milestone-2.php
+++ milestone-2.php
@@ -54,41 +54,47 @@
 
 
 		<div class="container">
+			<!-- Example row of columns -->
 			<div class="row">
-				<div class="col-md-8">
-					<h4>NAME:</h4>
-					<p>Peter Peterson</p>
+				<div class="col-lg-8">
+					<p><h4>Name:</h4>Pete Peterson</p>
 
-						<h4>Profession:</h4>
-						<p>15 yr as a small business owner of "petes bird shop” pete is very well respected and long standing professional in his community who takes his job seriously. Pete business caters to all pets needs and also has a small aquarium section a reptile habitat and a aviary. Pete relies on his community for his business to thrive, Pete's community also depends on "Pete's pet shop" for the services and products that he has been provides.</p>
+					<h4>Age:</h4>
+					<p>45</p>
+
+					<h4>Profession</h4>
+					<p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+
+					<h4>Technology:</h4>
+					<p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+
+					<h4>Attitides & Behaviors:</h4>
+					<p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+
+					<h4>Frustrations and Needs:</h4>
+					<p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+
+					<h4>Goals:</h4>
+					<p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
 
 
-						<h4>Technology:</h4>
-						<p> Pete uses windows 8 at home and at work. he has a surface pro at home and uses a hp desktop in  his office at the pet shop. Peter uses a iPhone 4s with service from sprint as his personal mobile device.</p>
-
-
-
-						<h4>Attitude & Behaviours:</h4>
-						<p>He has fun ago work likes talking to customers and enjoys seeing the excitement children when  they are browsing through the pet shop. Peter has fallen into a good smooth flow of how his business runs. He has minor speed bumps thrown at him from time to time but nothing that he can not handle with his experience.The hiring the occasional knowledgeable employee can sometime be the hardest part of his job. Peter likes how his interactive, bright and colorful pet shop put a little pep in the step and a smile on the face of his customer when they walk into his shop. FromPeter has the basic typical  "end user" knowledge of how the internet works. He does not understand the intricacy of the development or core functioning of a website.</p>
-
-						<h4>Frustrations & Needs:</h4>
-						<p>Pete receives questions almost daily about his non working website that throws a 404 error and has been on stall for the last 8yrs. Pete keeps renewing his domain year after year with hopes of oneway having a website for his business. Pete does not like the idea of having someone in another city, state or county to build his website no matter how affordable it seems. Pete does not want or need to sell products online but he does to showcase his business, services. Pete wants a website that is comparatively colorful, modern fun and appealing as his brick and mortar business. Pete need someone that take his vision and turn it into a reality. Peter wants to be able to see the face and shake the hand of the person that he will be obtain services from. He seeks a local developer/business in his home town and not miles and miles away where there is no person contact.</p>
-
-
-						<h4>Goals:</h4>
-						<p>pete is mostly concerned with his site being able to match the look and feel of his brick and mortar shop. pete will make his decision based on his belief if her feels that he will be able to work with the person/developer, and will be reassure his decision by looking over the portfolio of previous work that the developer/business has provides for other businesses.</p>
 				</div>
-			</div>
-		</div>
-
 				<div class="col-md-4">
 					<h2>Use Case</h2>
 					<p>this is just a place holder with text for the use case that i am developing. i really hope that it is use full and easy to understand. SOmetimes when i write stuff
-					i dont understand half of th things that i put in the page. Once i dont even know this but i added my personal info ya totally stupid i just spaced out adn start to type what
-					i was thinking about. i will do it now but i willl not add personal info....chinese food sounds good right now, but i know it will be a mistake later after it digests.
-					what ever happened to the dog that almost raped my cousin in 1996????? why does my dad always have a pissed off look on his face even when he is laughing.
-					i cant stop listening to this danm selena gomez song. take away yout hings and go you cant take back ehat you said, ive heard it a million times. i dont belive,
-						i dont belive that you left me in pieces. why is her head so small????? </p>
+						i dont understand half of th things that i put in the page. Once i dont even know this but i added my personal info ya totally stupid i just spaced out adn start to type what
+						i was thinking about. i will do it now but i willl not add personal info....chinese food sounds good right now, but i know it will be a mistake later after it digests.
+						what ever happened to the dog that almost raped my cousin in 1996????? why does my dad always have a pissed off look on his face even when he is laughing.
+						i cant stop listening to this danm selena gomez song. take away yout hings and go you cant take back ehat you said, ive heard it a million times. i dont belive,
+						i dont belive that you left me in pieces. why is her head so small?????</p>
+
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+			</div>
+
+
+					<h2>Use Case</h2>
+					<p> </p>
 
 			</div>
 		</div>
