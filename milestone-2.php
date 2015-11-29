a75c0ca21b68cf6f14e01a713c72a590499f5042
Carlos Beraun <cberaun2@cnm.edu>
2015-11-29 11:33:55 -0700

    deleted pop up button --->(milestone-2.php)

--- milestone-2.php
+++ milestone-2.php
@@ -103,17 +103,27 @@
 					<p><a class="btn btn-default" href="keyint001.jpg" role="button">View Key Interactions&raquo;</a></p>
 
 
-					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Popup image</button>
+					<!-- Button trigger modal -->
+					<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+						Launch demo modal
+					</button>
 
-					<div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<!-- Modal -->
+					<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 						<div class="modal-dialog">
 							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+									<h4 class="modal-title" id="myModalLabel">Modal title</h4>
+								</div>
 								<div class="modal-body">
-									<img src="keyint001.jpg" class="img-responsive">
+									...
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+									<button type="button" class="btn btn-primary">Save changes</button>
 								</div>
 							</div>
-						</div>
-					</div>
 
 
 
