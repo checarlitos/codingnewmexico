450f72daa7c6e8ce818079fa574e1cb3b210801a
Carlos Beraun <cberaun2@cnm.edu>
2015-11-29 21:56:49 -0700

    added image and button --->(milestone-2.php)

--- milestone-2.php
+++ milestone-2.php
@@ -122,12 +122,12 @@
 				<p>This site will have 4 pages. These pages will hold the content for the codingnewmexico.com site.</p>
 				<img class="img-responsive" src="keyint001.jpg">
 
-				<div> <ol><h2>
+				<div> <ol><h4>
 						<li>Landing Page</li>
 						<li>Home Page/portfolio</li>
 						<li>Contact Form</li>
 						<li>Profile</li>
-						</h2></ol>
+						</h4></ol>
 
 
 
