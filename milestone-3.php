af274ef5780735da9e2a00562bd427230c37d375
Carlos Beraun <cberaun2@cnm.edu>
2015-12-14 15:05:57 -0700

    Added milestone 3 page --->(milestone-2.php)

--- milestone-3.php
+++ milestone-3.php
@@ -60,28 +60,15 @@
 			<!-- Example row of columns -->
 			<div class="row">
 				<div class="col-md-12">
-					<h2>Persona</h2>
-					<p><h4>Name:</h4>Pete Peterson</p>
+					<h2>Mobile device wire frame.</h2>
 
-					<h4>Age:</h4>
-					<p>45</p>
-
-
-					<h4>Profession</h4>
-					<p>15 yr as a small business owner of "petes bird shop” pete is very well respected and long standing professional in his community who takes his job seriously. Pete business caters to all pets needs and also has a small aquarium section a reptile habitat and a aviary. Pete relies on his community for his business to thrive. Petes community also depends on Pete pet shop for the services and products that he provides.</p>
-
-					<h4>Technology:</h4>
-					<p>Pete uses windows 8 at home and at work. he has a surface pro at home and uses a hp desktop in  his office at the pet shop. Peter uses a iPhone 4s with service from sprint as his personal mobile device.</p>
-
-					<h4>Attitudes & Behaviors:</h4>
-					<p>He has fun ago work likes talking to customers and enjoys seeing the excitement children when  they are browsing through the pet shop. Peter has fallen into a good smooth flow of how his business runs. He has minor speed bumps thrown at him from time to time but nothing that he can not handle with his experience.The hiring the occasional knowledgeable employee can sometime be the hardest part of his job. Peter likes how his interactive, bright and colorful pet shop put a little pep in the step and a smile on the face of his customer when they walk into his shop. FromPeter has the basic typical  "end user" knowledge of how the internet works. He does not understand the intricacy of the development or core functioning of a website.</p>
-
-					<h4>Frustrations and Needs:</h4>
-					<p> Pete receives questions almost daily about his non working website that throws a 404 error and has been on stall for the last 8yrs. Pete keeps renewing his domain year after year with hopes of oneway having a website for his business. Pete does not like the idea of having someone in another city, state or county to build his website no matter how affordable it seems. Pete does not want or need to sell products online but he does to showcase his business, services. Pete wants a website that is comparatively colorful, modern fun and appealing as his brick and mortar business. Pete need someone that take his vision and turn it into a reality. Peter wants to be able to see the face and shake the hand of the person that he will be obtain services from. He seeks a local developer/business in his home town and not miles and miles away where there is no person contact. </p>
-
-					<h4>Goals:</h4>
-					<p>pete is mostly concerned with his site being able to match the look and feel of his brick and mortar shop. pete will make his decision based on his belief if her feels that he will be able to work with the person/developer, and will be reassure his decision by looking over the portfolio of previous work that the developer/business has provides for other businesses..</p>
-
+					<div> <ol><h4>
+								<li>Landing Page</li>
+								<li>Home Page/portfolio</li>
+								<li>Contact Form</li>
+								<li>Profile</li>
+							</h4></ol>
+					</div>
 
 				</div>
 
