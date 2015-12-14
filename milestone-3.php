552e35a07423fbf10cf4cb338f639a333a5ab0c2
Carlos Beraun <cberaun2@cnm.edu>
2015-12-14 13:46:55 -0700

    Added milestone 3 page --->(milestone-2.php)

---
+++ milestone-3.php
@@ -0,0 +1,140 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/html">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="x-ua-compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description">
+		<meta name="author">
+
+				<link rel="icon" href="../../favicon.ico">
+
+		<title> Coding New Mexico</title>
+
+				<!-- Bootstrap core CSS -->
+				<!-- Latest compiled and minified CSS -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
+				<!-- Optional theme -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+
+				<!-- Font Awsome----->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+				<!--custom css------>
+				<link rel="stylesheet" href="//css/miletwo.css">
+
+				<!-- Latest compiled and minified JavaScript -->
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
+	</head>
+
+	<body>
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only"> </span>
+						</button>
+							<a class="navbar=brand" href="milestone-2.php">MILEstone-2 for Carlos Beraun</a>
+					</div>
+					<div id="navbar" class="navbar-collapse collapse">
+						<form class="navbar-form navbar-right">
+
+						</form>
+					</div>
+				</div>
+			</nav>
+
+
+		<!----Message Area----->
+			<div class="jumbotron">
+			<div class="container">
+				<h1>MILEstone-2</h1>
+				<p>"The purpose of Milestone 2 is to map out in further detail the User Experience and Interaction Design for the PWP prior to creating a mockup/wireframe, and commencing development. Focus will be placed on developing the foundation for creating a Minimum Viable Product."</p>
+			</div>
+		</div>
+
+
+		<div class="container">
+			<!-- Example row of columns -->
+			<div class="row">
+				<div class="col-lg-8">
+					<h2>Persona</h2>
+					<p><h4>Name:</h4>Pete Peterson</p>
+
+					<h4>Age:</h4>
+					<p>45</p>
+
+
+					<h4>Profession</h4>
+					<p>15 yr as a small business owner of "petes bird shop” pete is very well respected and long standing professional in his community who takes his job seriously. Pete business caters to all pets needs and also has a small aquarium section a reptile habitat and a aviary. Pete relies on his community for his business to thrive. Petes community also depends on Pete pet shop for the services and products that he provides.</p>
+
+					<h4>Technology:</h4>
+					<p>Pete uses windows 8 at home and at work. he has a surface pro at home and uses a hp desktop in  his office at the pet shop. Peter uses a iPhone 4s with service from sprint as his personal mobile device.</p>
+
+					<h4>Attitudes & Behaviors:</h4>
+					<p>He has fun ago work likes talking to customers and enjoys seeing the excitement children when  they are browsing through the pet shop. Peter has fallen into a good smooth flow of how his business runs. He has minor speed bumps thrown at him from time to time but nothing that he can not handle with his experience.The hiring the occasional knowledgeable employee can sometime be the hardest part of his job. Peter likes how his interactive, bright and colorful pet shop put a little pep in the step and a smile on the face of his customer when they walk into his shop. FromPeter has the basic typical  "end user" knowledge of how the internet works. He does not understand the intricacy of the development or core functioning of a website.</p>
+
+					<h4>Frustrations and Needs:</h4>
+					<p> Pete receives questions almost daily about his non working website that throws a 404 error and has been on stall for the last 8yrs. Pete keeps renewing his domain year after year with hopes of oneway having a website for his business. Pete does not like the idea of having someone in another city, state or county to build his website no matter how affordable it seems. Pete does not want or need to sell products online but he does to showcase his business, services. Pete wants a website that is comparatively colorful, modern fun and appealing as his brick and mortar business. Pete need someone that take his vision and turn it into a reality. Peter wants to be able to see the face and shake the hand of the person that he will be obtain services from. He seeks a local developer/business in his home town and not miles and miles away where there is no person contact. </p>
+
+					<h4>Goals:</h4>
+					<p>pete is mostly concerned with his site being able to match the look and feel of his brick and mortar shop. pete will make his decision based on his belief if her feels that he will be able to work with the person/developer, and will be reassure his decision by looking over the portfolio of previous work that the developer/business has provides for other businesses..</p>
+
+
+				</div>
+				<div class="col-md-4">
+					<h2>Use Case</h2>
+					<p>Pete Peterson has looks over a flyer that he has received in the mail from codingnewmexico.com. He is intrigued, he begins to do a little more research on the coding New Mexico company using his mobile device.
+
+						<p>Pete continues to read over and explore the codingnewmexico.com site and see what services are provided. As Pete reads from page to page, he starts to feel inspired by the website to create a web presence for his own business. Looking through the site's pages he looks over previous work done by codingnewmexico.com Pete begins to mentally build a picture of what he would like to have in a site for himself. Pete's interest has peaked and has drawn him to contacting codingnewmexico.com to schedules a appointment and meet Carlos at his Pet Store.
+						</p>
+
+						<p>On a midweek afternoon day Pete decides to contact codingnewmexico.com to schedule an appointment. Pete pulls up the codingnewmexico.com site using his office computer with a 22" monitor. Pete then sends an email using a contact form on the codingnewmexico.com landing page, he gives himself another quick tour and as he continues to read over the website he see's that on he is able to associate with some of the business points showcased by codingnewmexico.com's business mission found on Carlos's profile page.
+						</p>
+
+						<!---- not needed content
+						<p>Pete and Carlos have a general dialogue regarding some of Coding New Mexico's previous clients and current services that Coding New Mexico can provided to Pete, from web development and design to personal software and web applications. Carlos proceeds to ask pete what type of services he is need of, A more extensive portfolio and samples are shown to help create inspiration for his own web site. Carlos then asks Pete to creatively explain a base line idea of what type of work Pete is seeking for his store website. While still inspired Carlos then provides Pete with a modest estimate and time frame for delivery of a final product. During the entire communication process Carlos has established that if Pete chooses to obtain Coding New Mexico's service's he is assured that his business would be in good hands.
+						</p>
+						----->
+
+					<!-----button
+					<p><a class="btn btn-default" href="keyint001.jpg" role="button">Click to enlarge&raquo;</a></p>
+					---->
+
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+
+			</div>
+		</div>
+
+
+
+		<div class="jumbotron">
+			<div class="container">
+				<h2>Sitemap</h2>
+				<p>This site will have 4 pages. These pages will hold the content for the codingnewmexico.com site.</p>
+				<img class="img-responsive" src="keyint001.jpg">
+
+				<div> <ol><h4>
+						<li>Landing Page</li>
+						<li>Home Page/portfolio</li>
+						<li>Contact Form</li>
+						<li>Profile</li>
+						</h4></ol>
+
+
+
+				</div>
+			</div>
+		</div>
+
+
+	</body>
+</html>
