7d04529c9338e720b12f03d66910df1717b9249c
Carlos Beraun <cberaun2@cnm.edu>
2015-12-15 15:56:02 -0700

    Added header block --->(index.php)

--- index.php
+++ index.php
@@ -63,6 +63,20 @@
 		</nav>
 
 
+	<header>
+		<div class="header-content">
+			<div class="header-content-inner">
+				<h1>Coding New Mexico. </h1>
+				<hr>
+				<p>We provide BIG affordable media solutions small businesses</p>
+				<a href="#Link_goes-Here" class="btn btn-primary btn-xl page-scroll">Learn More Here</a>
+			</div>
+		</div>
+	</header>
+
+
+
+
 
 
 
