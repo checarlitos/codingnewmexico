3206903d9e65f00ff3b8522ee5fe8cda67c8bd10
Carlos Beraun <cberaun2@cnm.edu>
2015-12-15 01:15:45 -0700

    Started adding header block--->(index.php)

--- index.php
+++ index.php
@@ -1,6 +1,51 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/html">
 	<head>
 		<meta charset="UTF-8">
-		</head>
-	</html>
+		<meta http-equiv="x-ua-compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description">
+		<meta name="Carlos Beraun cbabq505@gmail.com">
+
+		<!-----
+		<link rel="icon" href="../../favicon.ico">
+		------->
+
+		<title> Coding New Mexico</title>
+
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+		<!--custom fonts--->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+		<!--Plugin CSS -->
+		<link rel="stylesheet" href="css/>animate.min.css" type="text/css">
+
+		<!--custom CSS -->
+		<link rel="stylesheet" href="css/creative.min.css" type="text/css"
+
+		<!--custom css------>
+		<link rel="stylesheet" href="//css/miletwo.css">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
+	</head>
+
+<body id="page-top">
+
+		<nav id="MainNav" class="navbar navbar-default navbar-fixed-top">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">
+
+						</span>
+					</button>
+				</div>
+			</div>
+		</nav>
+</body>
