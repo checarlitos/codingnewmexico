ee84025f6fbd12c8503f952adc2229c7f202b047
Carlos Beraun <cberaun2@cnm.edu>
2015-12-18 11:50:51 -0700

    iam done!!!!!--->(index.php)

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-<!DOCTYPE HTML>
-<html xmlns="http://www.w3.org/1999/html">
+
+<!DOCTYPE html>
+<html lang="en">
+
 	<head>
 
 		<meta charset="utf-8">
@@ -22,279 +24,293 @@
 		<link rel="stylesheet" href="css/animate.min.css" type="text/css">
 
 		<!-- Custom CSS -->
-		<link rel="stylesheet" href="css/creative.css" type="text/css">
+		<link rel="stylesheet" href="css/style.css" type="text/css">
 
-
+		<!-- IE SUPPORT---->
 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-
+		<!---support and plug in ends here-->
 
 	</head>
 
-<body id="page-top">
+	<body id="page-top">
 
-		<nav id="MainNav" class="navbar navbar-default navbar-fixed-top">
+
+		<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
 			<div class="container-fluid">
+
+				<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="navbar-header">
-
-					<!------change data-target to an id with a custom name------>
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-								<span class="sr-only"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"><</span>
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
 					</button>
-
-					<a class="navbar-brand page-scroll" href="#page-top">
-						CodingNewMexico.com
-						<img alt="Brand" src="...ziaimage.png">
-					</a>
+					<a class="navbar-brand page-scroll" href="#page-top">Codingnewmexico.com</a>
+					<img alt="Brand" class="zialogo" src="img/ziaimage.png">
 				</div>
 
-				<!----header buttons------->
-				<!------change data-target to an id with a custom name------>
-				<div class=" collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav navbar-right">
-						<li><a class="page-scroll">About us</a></li>
-						<li><a class="page-scroll">Portfolio</a></li>
-						<li><a class="page-scroll">Contact</a></li>
+						<li>
+							<a class="page-scroll" href="#about">About</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#services">Services</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#portfolio">Portfolio</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#contact">Contact</a>
+						</li>
 					</ul>
-				</div>
-
-			</div>
+				</div><!--navbar-ends -->
+			</div><!---container ends---->
 		</nav>
 
-
-	<header>
-		<div class="header-content">
-			<div class="header-content-inner">
-				<h1>Coding New Mexico. </h1>
-				<hr>
-				<p>We provide affordable media solutions for small businesses</p>
-				<a href="#Link_goes-Here" class="btn btn-primary btn-xl page-scroll">Learn More Here</a>
+		<header><!---top page and image start--->
+			<div class="header-content">
+				<div class="header-content-inner">
+					<h1>Coding New Mexico.<hr>Developing ideas into realities.</h1>
+					<hr>
+					<p><h3>We do more than build website and apps...... we make dreams come true for business throughout New Mexico.</h3> </p>
+					<a href="#about" class="btn btn-primary btn-xl page-scroll">LEARN MORE HERE</a>
+				</div>
 			</div>
-		</div>
-	</header>
+		</header><!----top page end--->
 
-	<section class="bg-primary" id="about">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2 text-center">
-						<h2 class="service-heading">We can help your business</h2>
+		<section class="bg-primary" id="about"><!-----Middle orange bar---->
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2 text-center">
+						<h1 class="section-heading">We can help your business!</h1>
 						<hr class="light">
-						<p class="text-fade">Coding New Mexico can provide everything a small business needs in media solutions. Web Sites, programing and Graphic design are only some of our specialties we also provide any print media resource available under the sun.</p>
-						<a href="#linkgoes here" class="btn btn-default btn-x1">Get started today</a>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<section id="services">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12 text-center">
-					<h2 class="section-heading">Our Services</h2>
-					<hr class="primary">
-				</div>
-			</div>
-		</div>
-
-		<div class="container">
-			<div class="row">
-				<div class="col-md-3 col-md-6 text-center">
-					<div class="service-box">
-						<i class="fa fa-4x fa-mobile wow bounceIn text-primary"></i>
-						<i class="fa fa-4x fa-desktop wow bounceIn text-primary"></i>
-						<h3>Web & App Development</h3>
-						<p class="text-muted">All Our websites and Apps are built to deliver and maximize your brand's image across all major browsers and mobile devices.</p>
-					</div>
-				</div>
-				<div class="col-md-3 col-md-6 text-center">
-					<div class="service-box">
-						<i class="fa fa-4x fa-facebook-official wow bounceIn text-primary" data-wow-delay=".1s"></i>
-						<i class="fa fa-4x fa-twitter-square wow bounceIn text-primary" data-wow-delay=".1s"></i>
-						<h3>Social Media Management</h3>
-						<p class="text-muted">We will manage your businesses social web presence and develop social advertising campaigns.</p>
-					</div>
-				</div>
-				<div class="col-md-3 col-md-6 text-center">
-					<div class="service-box">
-						<i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
-						<h3>Graphic Design & Printing</h3>
-						<p class="text-muted">We produce amazing graphic design media for the web, video and printing. Coding New Mexico now offers a large range of full service printing options.</p>
-					</div>
-				</div>
-				<div class="col-md-3 col-md-6 text-center">
-					<div class="service-box">
-						<i class="fa fa-4x fa-television wow bounceIn text-primary" data-wow-delay=".3s"></i>
-
-						<h3>Advertising & Video</h3>
-						<p class="text-muted">From billboard campaigns to tv commercials, Coding New Mexico provides top quality advertising solutions for all business sizes .</p>
+						<p class="text-faded"><h2>Coding New Mexico can provide everything a small business needs in media solutions.
+							Web Sites, programing and Graphic design are only some of our specialties. Coding new mexico also provides
+							video advertising and tv/web commercials and We also have a large range of print media resource available.</h2></p>
+						<!-------button
+						<a class="btn btn-default btn-xl" data-toggle="modal" data-target="#myModal">CONTACT US</a>
+						------>
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section><!------end of middle Bar----->
 
-	<section class="no-padding" id="portfolio">
-		<div class="container-fluid">
-			<div class="row no-gutter">
-				<div class="col-md-4 col-sm-6">
-					<a href="#link goes here " class="portfolio-box">
-							<img src="#IMAGE GOES HERE 1" class="img-responsive" alt="">
-						<div class="portfolio-box-caption">
-							<div class="portfolio-box-caption-contest">
-								<div class="project-category text-fade">
-								Sample of work
-							</div>
-
-							<div class="project-name">
-								Portfolio Sample
-							</div>
-							</div>
-						</div>
-					</a>
+		<section id="services"><!-----start of our service block------->
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<h1 class="section-heading">Our Services</h1>
+						<hr class="primary">
+					</div>
 				</div>
-
-				<div class="col-md-4 col-sm-6">
-					<a href="#link goes here" class="portfolio-box">
-						<img src="#IMAGE GOES HERE 2" class="img-responsive" alt="">
-						<div class="portfolio-box-caption">
-							<div class="portfolio-box-caption-contest">
-								<div class="project-category text-fade">
-									Sample of work
-								</div>
-
-								<div class="project-name">
-									Portfolio Sample
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-
-				<div class="col-md-4 col-sm-6">
-					<a href="#link goes here" class="portfolio-box">
-						<img src="#IMAGE GOES HERE 3" class="img-responsive" alt="">
-						<div class="portfolio-box-caption">
-							<div class="portfolio-box-caption-contest">
-								<div class="project-category text-fade">
-									Sample of work
-								</div>
-
-								<div class="project-name">
-									Portfolio Sample
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-
-
-				<div class="col-md-4 col-sm-6">
-					<a href="#link goes here" class="portfolio-box">
-						<img src="#IMAGE GOES HERE4" class="img-responsive" alt="">
-						<div class="portfolio-box-caption">
-							<div class="portfolio-box-caption-contest">
-								<div class="project-category text-fade">
-									Sample of work
-								</div>
-
-								<div class="project-name">
-									Portfolio Sample
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-
-
-				<div class="col-md-4 col-sm-6">
-					<a href="#link goes here" class="portfolio-box">
-						<img src="#IMAGE GOES HERE5" class="img-responsive" alt="">
-						<div class="portfolio-box-caption">
-							<div class="portfolio-box-caption-contest">
-								<div class="project-category text-fade">
-									Sample of work
-								</div>
-
-								<div class="project-name">
-									Portfolio Sample
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-
-
-				<div class="col-md-4 col-sm-6">
-					<a href="#link goes here" class="portfolio-box">
-						<img src="#IMAGE GOES HERE6" class="img-responsive" alt="">
-						<div class="portfolio-box-caption">
-							<div class="portfolio-box-caption-contest">
-								<div class="project-category text-fade">
-									Sample of work
-								</div>
-
-								<div class="project-name">
-									Portfolio Sample
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-
 			</div>
-		</div>
-	</section>
-
-	<aside class="bg-dark">
-		<div class="container text-center">
-			<div class="call-to-action">
-				<h2>Talk to some one today about how we can help you improve your web presence.</h2>
-				<a href="Link goes here" class="btn btn-default btn-xl wow tada">Contact Us</a>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 col-md-6 text-center">
+						<div class="service-box">
+							<i class="fa fa-4x fa-mobile wow bounceIn text-primary"></i>
+							<i class="fa fa-4x fa-desktop wow bounceIn text-primary"></i>
+							<h3>Web & App Development</h3>
+							<p class="text-muted">All Our websites and Apps are built to deliver and maximize your brand's image across all major browsers and mobile devices.</p>
+						</div>
+					</div>
+					<div class="col-md-3 col-md-6 text-center">
+						<div class="service-box">
+							<i class="fa fa-4x fa-facebook-official wow bounceIn text-primary" data-wow-delay=".1s"></i>
+							<i class="fa fa-4x fa-twitter-square wow bounceIn text-primary" data-wow-delay=".1s"></i>
+							<h3>Social Media Management</h3>
+							<p class="text-muted">We will manage your businesses social web presence and develop social advertising campaigns.</p>
+						</div>
+					</div>
+					<div class="col-md-3 col-md-6 text-center">
+						<div class="service-box">
+							<i class="fa fa-4x fa-paint-brush wow bounceIn text-primary" data-wow-delay=".2s"></i>
+							<i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
+							<h3>Graphic Design & Printing</h3>
+							<p class="text-muted">We produce amazing graphic design media for the web, video and printing. Coding New Mexico now offers a large range of full service printing options.</p>
+						</div>
+					</div>
+					<div class="col-md-3 col-md-6 text-center">
+						<div class="service-box">
+							<i class="fa fa-4x fa-video-camera wow bounceIn text-primary" data-wow-delay=".3s"></i>
+							<h3>Advertising & Video</h3>
+							<p class="text-muted">From billboard campaigns to tv commercials, Coding New Mexico provides top quality advertising solutions for all business sizes.</p>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
-	</aside>
+		</section><!------end of our service block--------->
 
-	<section id="contact">
-		<div class="container text-center">
-			<div class="row">
-				<div class="col-lg-8 col-lg-offset-2 text-center">
-					<h2 class="section-heading">Have a Question???</h2>
-					<hr class="primary">
-					<p>Give us a call or send us a email. We will be happy to help answer any questions about our services.</p>
+		<section class="no-padding" id="portfolio"><!--------start of sample portfolio---------->
+			<div class="container-fluid">
+				<div class="row no-gutter">
+					<div class="col-lg-4 col-sm-6">
+						<a href="http://www.aztecasfutbolclub.com/" class="portfolio-box">
+							<img src="img/portfolio/aztecas.jpg" class="img-responsive" alt="">
+							<div class="portfolio-box-caption">
+								<div class="portfolio-box-caption-content">
+									<div class="project-category text-faded">
+										Aztecas F.C
+									</div>
+									<div class="project-name">
+										LOGO & WEBSITE
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<a href="http://www.hotyogainfusion.com" class="portfolio-box">
+							<img src="img/portfolio/hyiweb.jpg" class="img-responsive" alt="">
+							<div class="portfolio-box-caption">
+								<div class="portfolio-box-caption-content">
+									<div class="project-category text-faded">
+										hotyogainfusion.com
+									</div>
+									<div class="project-name">
+										WEBSITE
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<a href="#" class="portfolio-box">
+							<img src="img/portfolio/bbanmweb.jpg" class="img-responsive" alt="">
+							<div class="portfolio-box-caption">
+								<div class="portfolio-box-caption-content">
+									<div class="project-category text-faded">
+										BBANM
+									</div>
+									<div class="project-name">
+										LOGO, PRINTING & WEBSITE
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<a href="#" class="portfolio-box">
+							<img src="img/portfolio/ridgetop.jpg" class="img-responsive" alt="">
+							<div class="portfolio-box-caption">
+								<div class="portfolio-box-caption-content">
+									<div class="project-category text-faded">
+										Ridge top Plumbing
+									</div>
+									<div class="project-name">
+										LOGO & CUSTOM STICKERS
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<a href="#" class="portfolio-box">
+							<img src="img/portfolio/poco.jpg" class="img-responsive" alt="">
+							<div class="portfolio-box-caption">
+								<div class="portfolio-box-caption-content">
+									<div class="project-category text-faded">
+										Poquito Biscochitos
+									</div>
+									<div class="project-name">
+										WEBSITE
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<a href="#" class="portfolio-box">
+							<img src="img/portfolio/hbb.jpg" class="img-responsive" alt="">
+							<div class="portfolio-box-caption">
+								<div class="portfolio-box-caption-content">
+									<div class="project-category text-faded">
+										Help Bail Bonds
+									</div>
+									<div class="project-name">
+										CUSTOM PRINTING
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
 				</div>
-				<div class="col-lg-4 col-lg-offset-2 text-center">
-					<i class="fa fa-phone fa-3x wow bounceIn"></i>
-					<p>(505)220-5290</p>
-				</div>
-				<div class="col-lg-4 text-center">
-					<i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
-					<p><a href="mailto:505csb@gmail.com">505csb@gmail.com</a></p>
-				</div>
-
 			</div>
-		</div>
-	</section>
+		</section>
 
+		<aside class="bg-strip">
+			<div class="container text-center">
+				<div class="call-to-action">
+					<h2>Want to see more of our work, check out our full profile.</h2>
+					<a href="#" class="btn btn-primary btn-xl wow tada">Click Here</a>
+				</div>
+			</div>
+		</aside><!--------end of sample portfolio---------->
 
+		<section id="contact"><!------start of contact and form--------->
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2 text-center">
+						<h2 class="section-heading">Give us a call or send us a e-mail</h2>
+						<hr class="primary">
+						<p></p>
+					</div>
+					<div class="col-lg-4 col-lg-offset-2 text-center">
+						<i class="fa fa-phone fa-3x wow bounceIn"></i>
+						<p>505-220-5290</p>
+					</div>
+					<div class="col-lg-4 text-center">
+						<i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+						<p><a href="mailto:505csb@gmail.com.com">505csb@gmail.com</a></p>
+					</div>
+				</div>
+				</div>
 
+			<div class="vertical-center">
+				<div class="container"
+			<div class="col-sm-6">
+				<div class="well">
+					<form action="">
+						<div class="form-group">
+							<div class="form-group">
+								<label for="contactName">Name</label>
+								<input type="text" class="form-control" id="contactName" name="contactName" placeholder="Your Name">
+							</div>
+							<label for="contactEmail">Email address</label>
+							<input type="email" class="form-control" id="contactEmail" placeholder="your email address" name="contactEmail">
+						</div>
+						<div class="form-group">
+							<label for="contactText">Message</label>
+							<textarea class="form-control" name="name" maxlength="500" placeholder="your message here"></textarea>
+						</div>
+						<button type="submit" class="btn btn-info">Submit</button>
+					</form>
+				</div>
+			</div>
+			</div>
+			</div>
+		</section><!-------end of contact and form------->
 
-	<!----Jquery------>
-	<script src="js/jquery.js"></script>
+		<!--------------------plug-ins------------------------------>
+		<!-- jQuery -->
+		<script src="js/jquery.js"></script>
 
-	<!---bootstrap core javaScript---->
-	<script src="js/bootstrap.min.js"></script>
+		<!-- Bootstrap Core JavaScript -->
+		<script src="js/bootstrap.min.js"></script>
 
-	<!----plugin javaScript----->
-	<script src="js/jquery.easing.min.js"></script>
-	<script src="js/Jquery.fittext.js"></script>
-	<script src="js.wow.min.js"></script>
+		<!-- Plugin JavaScript -->
+		<script src="js/jquery.easing.min.js"></script>
+		<script src="js/jquery.fittext.js"></script>
+		<script src="js/wow.min.js"></script>
 
-	<!----custom theme JavaScript----->
-	<script src="js/creative.js"></script>
+		<!-- Custom Theme JavaScript -->
+		<script src="js/creative.js"></script>
+		<!-----------------end of plug ins------------------------->
 
-
-</body>
+	</body>
 
 </html>
--- pasta.php
+++ pasta.php
@@ -1,15 +1,8 @@
-<div class="col-md-8 col-md-offcet-2 text-center">
-	<h2 class="section-heading"> Have a Question???</h2>
-	<hr class="primary">
-	<p>Give us a call or send us a email and we will be happy to help answer any questions about our services.</p>
-</div>
-
-<div class="col-md-8 col-md-offset-2 text-center">
-	<i class="fa fa-phone fa-3x wow bounceIn"></i>
-	<p>505-220-5290</p>
-</div>
-
-<div class="col-md-4 text-center">
-	<i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay="1.s"></i>
-	<p><a href="mailto:cbabq505@gmail.com">cbabq505@gmail.com</a></p>
+<div class="header-content">
+	<div class="header-content-inner">
+		<h1>Coding New Mexico. </h1>
+		<hr>
+		<p>We provide affordable media solutions for small businesses</p>
+		<a href="#Link_goes-Here" class="btn btn-primary btn-xl page-scroll">Learn More Here</a>
+	</div>
 </div>
